cb756f1e8178815abc75f2d1b8bad9a38a63cef8
Haidar <haidarrmehsen@gmail.com>
2024-07-24 15:57:23 +0300

    Initial commit

---
+++ lib/flutter_cli_utils.dart
@@ -0,0 +1,10 @@
+/// flutter_cli_utils, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate flutter_cli_utils
+/// dart pub global activate flutter_cli_utils
+///
+/// # see usage
+/// flutter_cli_utils --help
+/// ```
+library;
