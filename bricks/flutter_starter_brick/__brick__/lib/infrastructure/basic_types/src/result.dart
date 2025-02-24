d077844bc3f321d3a62b8c333fdf47c98536e98c
Haidar <haidarrmehsen@gmail.com>
2025-02-24 18:28:12 +0200

    Added toString methods to result.dart

--- bricks/flutter_starter_brick/__brick__/lib/infrastructure/basic_types/src/result.dart
+++ bricks/flutter_starter_brick/__brick__/lib/infrastructure/basic_types/src/result.dart
@@ -13,6 +13,7 @@ sealed class Result<F, S> {
 
 class Success<S> extends Result<Never, S> {
   const Success._(this.data);
+
   final S data;
 
   @override
@@ -20,10 +21,14 @@ class Success<S> extends Result<Never, S> {
     required T Function(S data) success,
     required T Function(Never data) failure,
   }) => success(data);
+
+  @override
+  String toString() => 'Success{data: $data}';
 }
 
 class Failure<F> extends Result<F, Never> {
   const Failure._(this.data);
+
   final F data;
 
   @override
@@ -31,4 +36,7 @@ class Failure<F> extends Result<F, Never> {
     required T Function(Never data) success,
     required T Function(F data) failure,
   }) => failure(data);
+
+  @override
+  String toString() => 'Failure{data: $data}';
 }
