167692446ade734c1066f525a955fab47b270ce7
Haidar <haidarrmehsen@gmail.com>
2025-02-24 18:27:47 +0200

    Added toString methods to network_failure.dart

--- bricks/flutter_starter_brick/__brick__/lib/infrastructure/networking/http_client/src/network_failure.dart
+++ bricks/flutter_starter_brick/__brick__/lib/infrastructure/networking/http_client/src/network_failure.dart
@@ -1,25 +1,56 @@
 sealed class NetworkFailure {
   const NetworkFailure({this.statusCode, this.serverErrorMessage});
+
   final int? statusCode;
   final String? serverErrorMessage;
 }
 
 final class NetworkError extends NetworkFailure {
   const NetworkError({super.statusCode, super.serverErrorMessage});
+
+  @override
+  String toString() =>
+      'NetworkError{'
+          'statusCode: $statusCode, '
+          'serverErrorMessage: $serverErrorMessage}';
 }
 
 final class ServerError extends NetworkFailure {
   const ServerError({super.statusCode, super.serverErrorMessage});
+
+  @override
+  String toString() =>
+      'ServerError{'
+          'statusCode: $statusCode, '
+          'serverErrorMessage: $serverErrorMessage}';
 }
 
 final class TimeoutError extends NetworkFailure {
   const TimeoutError({super.statusCode, super.serverErrorMessage});
+
+  @override
+  String toString() =>
+      'TimeoutError{'
+          'statusCode: $statusCode, '
+          'serverErrorMessage: $serverErrorMessage}';
 }
 
 final class CancelError extends NetworkFailure {
   const CancelError({super.statusCode, super.serverErrorMessage});
+
+  @override
+  String toString() =>
+      'CancelError{'
+          'statusCode: $statusCode, '
+          'serverErrorMessage: $serverErrorMessage}';
 }
 
 final class UnknownError extends NetworkFailure {
   const UnknownError({super.statusCode, super.serverErrorMessage});
+
+  @override
+  String toString() =>
+      'UnknownError{'
+          'statusCode: $statusCode, '
+          'serverErrorMessage: $serverErrorMessage}';
 }
