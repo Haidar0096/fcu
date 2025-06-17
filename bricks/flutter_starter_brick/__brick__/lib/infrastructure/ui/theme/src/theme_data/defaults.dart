0caaa93cf14a4fe5d4fc9b674c9b386f922bf1c9
Haidar <haidarrmehsen@gmail.com>
2025-06-17 20:11:58 +0300

    Removed unused inputFieldBorderRadius from ThemeDefaults

--- bricks/flutter_starter_brick/__brick__/lib/infrastructure/ui/theme/src/theme_data/defaults.dart
+++ bricks/flutter_starter_brick/__brick__/lib/infrastructure/ui/theme/src/theme_data/defaults.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/widgets.dart';
-
 class ThemeDefaults {
-  static final BorderRadius inputFieldBorderRadius = BorderRadius.circular(8);
   static const double borderWidth = 1.3;
   static const double buttonHeight = 50;
   static const double buttonWidth = 100;
