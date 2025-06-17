7a257ce98eb14f5909f94e83711fa6703d30fcb8
Haidar <haidarrmehsen@gmail.com>
2025-06-18 01:33:02 +0300

    Refactor JokesCubit for improved error handling and state management
    - Introduced `CubitUtils` mixin for safe state emission (`emitIfNotClosed`).
    - Simplified error handling in `fetch()` method by providing a user-friendly error message for failed joke fetches.
    - Updated `JokesFailed` state to accept a string message instead of a `NetworkFailure` object.
    - Renamed `toUiModel()` to `toUi()` for consistency in the `JokeMapper`.

--- bricks/flutter_starter_brick/__brick__/lib/features/random_jokes/random_jokes_screen/src/blocs/jokes_cubit/jokes_cubit.dart
+++ bricks/flutter_starter_brick/__brick__/lib/features/random_jokes/random_jokes_screen/src/blocs/jokes_cubit/jokes_cubit.dart
@@ -1,26 +1,27 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:{{proj_name}}/shared/models/ui_models/ui_models.dart';
 import 'package:{{proj_name}}/features/random_jokes/random_jokes_screen/src/apis/jokes_api.dart';
 import 'package:{{proj_name}}/features/random_jokes/random_jokes_screen/src/models/mappers/joke_mapper.dart';
 import 'package:{{proj_name}}/features/random_jokes/random_jokes_screen/src/models/ui_models/ui_joke.dart';
+import 'package:{{proj_name}}/infrastructure/blocs/bloc_utils/bloc_utils.dart';
 import 'package:{{proj_name}}/infrastructure/dependency_injection/dependency_injection.dart';
-import 'package:{{proj_name}}/infrastructure/networking/http_client/http_client.dart';
 
 part 'jokes_state.dart';
 
 @Service()
-class JokesCubit extends Cubit<JokesState> {
+class JokesCubit extends Cubit<JokesState> with CubitUtils {
   JokesCubit(this._api) : super(JokesInitial());
-
   final JokesApi _api;
 
   Future<void> fetch() async {
     if (state.loading) return; // Prevent multiple fetches
-    emit(JokesLoading());
+    emitIfNotClosed(JokesLoading());
     final result = await _api.fetchRandomJoke();
     result.when(
-      success: (joke) => emit(JokesLoaded(joke.toUiModel())),
-      failure: (failure) => emit(JokesFailed(failure)),
+      success: (joke) => emitIfNotClosed(JokesLoaded(joke.toUi())),
+      failure:
+          (f) => emitIfNotClosed(
+        JokesFailed('Couldn’t fetch a joke – try again.'),
+      ),
     );
   }
 }
