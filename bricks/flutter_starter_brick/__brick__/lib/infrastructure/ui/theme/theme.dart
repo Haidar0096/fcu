a60580f20decc2d53f16199c09e3ff5e64b44f94
Haidar <haidarrmehsen@gmail.com>
2025-09-17 16:25:50 +0300

    feat: Add theme exports for improved theme management

--- bricks/flutter_starter_brick/__brick__/lib/infrastructure/ui/theme/theme.dart
+++
@@ -1,7 +0,0 @@
-export 'src/theme_cubit/theme_cubit.dart';
-export 'src/theme_data/color_scheme_extensions.dart';
-export 'src/theme_data/colors.dart';
-export 'src/theme_data/defaults.dart';
-export 'src/theme_data/theme_data.dart';
-export 'src/theme_data/typography.dart';
-export 'src/{{proj_name.snakeCase()}}_theme.dart';
