264a1a8d0579f3ae09fe8765691a6bdb289141d9
Haidar <haidarrmehsen@gmail.com>
2025-09-19 16:16:51 +0300

    feat: Add JokesApi for fetching random jokes from the API

--- bricks/flutter_starter_brick/__brick__/lib/features/random_jokes/random_jokes_screen/src/apis/jokes_api.dart
+++ bricks/flutter_starter_brick/__brick__/lib/features/random_jokes/random_jokes_screen/src/apis/jokes_api.dart
@@ -1,26 +1,22 @@
-import 'dart:async';
-
-import 'package:{{proj_name}}/dependency_injection/dependency_injection.dart';
 import 'package:{{proj_name}}/features/random_jokes/random_jokes_screen/src/models/dtos/joke_dto.dart';
-import 'package:{{proj_name}}/infrastructure/basic_types/basic_types.dart';
-import 'package:{{proj_name}}/infrastructure/dependency_injection/dependency_injection.dart';
-import 'package:{{proj_name}}/infrastructure/networking/http_client/http_client.dart';
+import 'package:{{proj_name}}/foundation/basic_types/basic_types.dart';
+import 'package:{{proj_name}}/foundation/networking/http_client/http_client.dart';
+import 'package:{{proj_name}}/foundation/networking/models/models.dart';
 
-@LazySingletonService()
-class JokesApi {
-  JokesApi(
-    @DIName(DependencyInjectionInstanceNames.jokesBackendHttpClient)
-    HttpClient httpClient,
-  ) : _httpClient = httpClient;
+/// API for random jokes operations
+final class JokesApi {
+  const JokesApi(
+    this._httpClient,
+  );
 
   final HttpClient _httpClient;
 
-  Future<Result<NetworkFailure, JokeDto>> fetchRandomJoke() async =>
+  /// Fetches a random joke from the API
+  Future<Result<NetworkFailure, JokeDto>> fetchRandomJoke() =>
       _httpClient.request(
         path: '/jokes/random',
         method: 'GET',
-        successResponseMapper:
-            (response) =>
-                JokeDto.fromJson(response.data as Map<String, dynamic>),
+        successResponseMapper: (response) =>
+            JokeDto.fromJson(response.data as Map<String, dynamic>),
       );
 }
