1d0f5c7100cd4f6d88c7065e5e76fb5553716c32
Haidar <haidarrmehsen@gmail.com>
2025-09-19 16:24:57 +0300

    feat: Update JokeDto to implement UiConvertibleDtoMixin for UI model conversion

--- bricks/flutter_starter_brick/__brick__/lib/features/random_jokes/random_jokes_screen/src/models/dtos/joke_dto.dart
+++ bricks/flutter_starter_brick/__brick__/lib/features/random_jokes/random_jokes_screen/src/models/dtos/joke_dto.dart
@@ -1,16 +1,31 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:{{proj_name}}/features/random_jokes/random_jokes_screen/src/models/ui_models/ui_joke.dart';
+import 'package:{{proj_name}}/foundation/ui/mixins/mixins.dart';
 
 part 'joke_dto.g.dart';
 
+/// DTO for joke API response
 @JsonSerializable()
-class JokeDto {
-  const JokeDto({required this.id, required this.value});
+final class JokeDto with UiConvertibleDtoMixin<UiJoke> {
+  const JokeDto({
+    required this.id,
+    required this.value,
+  });
 
   factory JokeDto.fromJson(Map<String, dynamic> json) =>
       _$JokeDtoFromJson(json);
 
+  /// Unique identifier for the joke
+  final String? id;
+
+  /// The joke content/text
+  final String? value;
+
   Map<String, dynamic> toJson() => _$JokeDtoToJson(this);
 
-  final String? id;
-  final String? value;
+  @override
+  UiJoke toUiModel() => UiJoke(
+    id: id ?? '',
+    content: value ?? '',
+  );
 }
--- bricks/flutter_starter_brick/__brick__/lib/features/random_jokes/random_jokes_screen/src/models/mappers/joke_mapper.dart
+++
@@ -1,6 +0,0 @@
-import 'package:{{proj_name}}/features/random_jokes/random_jokes_screen/src/models/dtos/joke_dto.dart';
-import 'package:{{proj_name}}/features/random_jokes/random_jokes_screen/src/models/ui_models/ui_joke.dart';
-
-extension JokeDtoMapper on JokeDto {
-  UiJoke toUiModel() => UiJoke(id: id ?? '', content: value ?? '');
-}
