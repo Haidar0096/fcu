6934bd7e091a90f5d50bca63c26448a73e2ba5d2
Haidar <haidarrmehsen@gmail.com>
2025-09-22 13:43:50 +0300

    feat: Refactor environment initialization to use unified Environment constants

--- bricks/flutter_starter_brick/__brick__/lib/main_staging.dart
+++ bricks/flutter_starter_brick/__brick__/lib/main_staging.dart
@@ -1,7 +1,7 @@
-import 'package:{{proj_name}}/infrastructure/environments/src/environments.dart';
+import 'package:{{proj_name}}/foundation/environments/environments.dart';
 import 'package:{{proj_name}}/main_common.dart';
 
 Future<void> main() async {
   // run the app with the staging environment
-  await mainCommon(const StagingEnvironment());
+  await mainCommon(Environment.staging);
 }
