1c62f54eab750088ef6a25db23939eef2628638c
Haidar <haidarrmehsen@gmail.com>
2025-09-22 20:31:47 +0300

    feat: Add color constants for theming and UI design

---
+++ bricks/flutter_starter_brick/__brick__/lib/foundation/ui/theme/src/theme_data/colors.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+const Color black = Color(0xFF000000);
+const Color white = Color(0xFFFFFFFF);
+
+const Color errorRed = Color(0xFFE74C3C);
+const Color successGreen = Color.fromARGB(255, 0, 225, 90);
+const Color inactiveGray = Color(0xFFCACACA);
+
+// Primary colors
+const Color primaryBlue = Color(0xFF3F5AAB);
+const Color primaryBlueContainer = Color(0xFFC6D1F5);
+
+// Secondary colors
+const Color secondaryTeal = Color(0xFF0B8793);
+const Color secondaryTealContainer = Color(0xFFA6D1D7);
+
+// Tertiary colors
+const Color tertiaryAmber = Color(0xFFFFB759);
+const Color tertiaryAmberContainer = Color(0xFFFFDDA6);
+
+// Surface colors
+const Color lightSurface = Color(0xFFF2F5F9);
+const Color darkSurface = Color(0xFF1E2229);
+const Color darkBackground = Color(0xFF0A0A0A);
+const Color darkGreyBubble = Color(0xFF3A4754);
+const Color mutedGray = Color(0xFF2A2A2A);
+
+// Outline colors
+const Color outlineGrey = Color(0xFF8A8F99);
