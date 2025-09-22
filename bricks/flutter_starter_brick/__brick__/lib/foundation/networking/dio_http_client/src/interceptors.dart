5f81a033d0e560ee222822ac7599c9eb224cc2aa
Haidar <haidarrmehsen@gmail.com>
2025-09-22 10:37:55 +0300

    feat: Add ErrorLoggerInterceptor for logging API request errors

---
+++ bricks/flutter_starter_brick/__brick__/lib/foundation/networking/dio_http_client/src/interceptors.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:{{proj_name}}/foundation/logging/logging.dart';
+import 'package:{{proj_name}}/foundation/networking/dio_http_client/src/build_api_error_reporting_message.dart';
+
+/// An interceptor for logging errors during API requests.
+class ErrorLoggerInterceptor extends Interceptor {
+  ErrorLoggerInterceptor({required this.appLogger, required this.errorLogger});
+
+  final AppLogger appLogger;
+  final ErrorLogger errorLogger;
+
+  @override
+  Future<void> onError(
+    DioException err,
+    ErrorInterceptorHandler handler,
+  ) async {
+    final message = buildApiErrorReportingMessageFromDioException(err);
+    appLogger.log(message);
+    await errorLogger.recordError(error: message, stackTrace: err.stackTrace);
+    return super.onError(err, handler);
+  }
+}
