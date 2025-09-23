fbbe395954884ca1814c44d82a5e0d508eeede31
Haidar <haidarrmehsen@gmail.com>
2025-09-23 10:40:28 +0300

    feat: Implement MainButton widget with customizable properties and styles

---
+++ bricks/flutter_starter_brick/__brick__/lib/foundation/ui/widgets/src/main_button.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:{{proj_name}}/foundation/ui/theme/theme.dart';
+import 'package:{{proj_name}}/foundation/ui/widgets/widgets.dart';
+
+class MainButton extends StatelessWidget {
+  const MainButton({
+    required this.text,
+    required this.onPressed,
+    this.height,
+    this.width,
+    super.key,
+  });
+
+  final String text;
+
+  final VoidCallback? onPressed;
+  final double? width;
+  final double? height;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = context.themeData.elevatedButtonTheme.style;
+    return CustomElevatedButton(
+      text: text,
+      width: width,
+      height: height,
+      style: ButtonStyle(
+        backgroundColor: WidgetStateProperty.resolveWith((states) {
+          if (states.contains(WidgetState.disabled)) {
+            return context.themeData.colorScheme.surfaceContainerHighest;
+          }
+          return context.themeData.colorScheme.primary;
+        }),
+        foregroundColor: WidgetStateProperty.resolveWith((states) {
+          if (states.contains(WidgetState.disabled)) {
+            return theme?.foregroundColor?.resolve(states);
+          }
+          return context.themeData.colorScheme.onPrimary;
+        }),
+        overlayColor: WidgetStateProperty.resolveWith((states) {
+          if (states.contains(WidgetState.disabled)) return null;
+          return context.themeData.colorScheme.onPrimary.withValues(
+            alpha: ThemeDefaults.buttonDisabledOverlayAlpha,
+          );
+        }),
+        shape: WidgetStatePropertyAll(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+              ThemeDefaults.buttonBorderRadius,
+            ),
+            side: const BorderSide(
+              color: Colors.transparent,
+              width: ThemeDefaults.borderWidth,
+            ),
+          ),
+        ),
+        textStyle: theme?.textStyle,
+      ),
+      onPressed: onPressed,
+    );
+  }
+}
